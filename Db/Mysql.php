01d4e868808ca7251b5138cc45e219f8972ca552
HY <https://github.com/07191216/cai.git>
2016-10-08 14:06:56 +0800

    第2次提交 4g

--- Db/Mysql.php
+++ Db/Mysql.php
@@ -1,6 +1,20 @@
 <?php
 namespace Db;
-class Mysql
-{
+class Mysql implements IDatabase{
+    function connect()
+    {
+        // TODO: Implement connect() method.
+    }
+
+    function query()
+    {
+        // TODO: Implement query() method.
+    }
+
+    function close()
+    {
+        // TODO: Implement close() method.
+    }
+
 
 }
--- Db/PDO.php
+++ Db/PDO.php
@@ -1,17 +1,20 @@
 <?php
 namespace Db;
-class PDO
+class PDO implements IDatabase
 {
     function connect()
     {
-        
+        // TODO: Implement connect() method.
     }
+
     function query()
     {
-        
+        // TODO: Implement query() method.
     }
+
     function close()
     {
-        
+        // TODO: Implement close() method.
     }
+
 }
